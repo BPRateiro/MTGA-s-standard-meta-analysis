0c021c0d67f004ab2ca886b4cee8f8b95b777e99
Bruno Pigari Rateiro <105450191+BPRateiro@users.noreply.github.com>
2023-05-12 17:51:06 -0300

    All card models written

--- src/main.py
+++ src/main.py
@@ -1,51 +1,100 @@
-"""Creates database and tables according to predetermined schema"""
-
 import os
 
 from sqlalchemy_utils import database_exists, create_database
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, update
 from sqlalchemy.engine import URL
 from sqlalchemy.orm import sessionmaker
+from sqlalchemy.sql import func
 
-from models import drop_all, create_all
-import writer as wr
+import pandas as pd
 
-def main():
-    """ Creates database and tables"""
+from models import Set, drop_all, create_all
+from card import get_card_information
+from raw import request_active
+
+
+def start_engine():
+    """Creates engine from URL, using environment variables"""
     url = URL.create(
-        drivername = "mysql+pymysql",
-        username = os.getenv('UNTAPPED_USERNAME'),
-        password = os.getenv('UNTAPPED_PASSWORD'),
-        host = os.getenv('UNTAPPED_HOST'),
-        port = os.getenv('UNTAPPED_PORT'),
-        database = "untapped"
+        drivername="mysql+pymysql",
+        username=os.getenv('UNTAPPED_USERNAME'),
+        password=os.getenv('UNTAPPED_PASSWORD'),
+        host=os.getenv('UNTAPPED_HOST'),
+        port=os.getenv('UNTAPPED_PORT'),
+        database="untapped"
     )
+    return create_engine(url)
 
-    engine = create_engine(url)
 
-    # Garantee that a database with certain name exists
-    if not database_exists(engine.url):
-        create_database(engine.url)
+def garantee_database(eng):
+    """Creates a database if one doesn't exist already"""
+    if not database_exists(eng.url):
+        create_database(eng.url)
+
+
+def write_set(session):
+    "Write set table"
+    # Get currently legal sets according to the website
+    format, active_sets = request_active()
+
+    # Get legal sets existing in the database
+    existing_sets = (session.query(Set.id, Set.rotated_on)
+                            .filter(Set.rotated_on.is_(None))
+                            .all())
+    existing_set_ids = {s[0] for s in existing_sets}
+
+    # Get list of legal sets that are not in database
+    new_sets = [s for s in active_sets if s not in existing_set_ids]
+
+    # Write these new sets on the database
+    if new_sets:
+        new_entities = [Set(id=set_id) for set_id in new_sets]
+        session.add_all(new_entities)
+        session.commit()
+
+    # Rotate sets that are not an active according to website
+    existing_ids_to_update = [s[0] for s in existing_sets
+                              if s[0] not in active_sets]
+    if existing_ids_to_update:
+        session.execute(
+            update(Set)
+            .where(Set.id.in_(existing_ids_to_update))
+            .values(rotated_on=func.now())
+        )
+        session.commit()
+
+    return format, new_sets
+
+
+def write_card(connection):
+    card_dataframes = get_card_information(request_active()[1])
+
+    for card_df in card_dataframes:
+        affected = card_df.to_sql(name=card_df.name,
+                                  con=connection,
+                                  if_exists='append')
+        print(f"Table '{card_df.name}' had {affected} new inclusions")
+
+
+if __name__ == '__main__':
+    engine = start_engine()  # Create engine
+    garantee_database(engine)  # Make sure 'untapped' db exists
 
     with engine.connect() as connection:
-        drop_all(engine) # For testing only
+        # drop_all(engine)  # For testing only
         create_all(engine)
 
         Session = sessionmaker(bind=engine)
         session = Session()
 
-        writer = wr.Writer()
-        writer.get_all()
+        # Write data
+        format, new_sets = write_set(session)
+        print(f'The current format is {format}')
 
-        writer.card.to_sql(
-            name = 'card',
-            con = engine,
-            if_exists = 'append',
-            index = True,
-            index_label = 'id'
-        )
+        if new_sets:
+            print(f'The following sets were included: {new_sets}')
+            write_card(connection)
+        else:
+            print(f'No new sets were included')
 
         session.commit()
-
-if __name__ == '__main__':
-    main()
--- src/models.py
+++ src/models.py
@@ -1,24 +1,27 @@
-from sqlalchemy import Column, Integer, String, Boolean, DateTime, Text, ForeignKey
+from sqlalchemy import (Column, Integer, String,
+                        Boolean, DateTime, Text, ForeignKey)
 from sqlalchemy.orm import declarative_base, relationship
 from sqlalchemy.sql import func
 
 Base = declarative_base()
+
 
 class Set(Base):
     __tablename__ = 'set'
 
     id = Column(String(3), primary_key=True)
     created_on = Column(DateTime, server_default=func.now())
-    rotated_on = Column(DateTime, onupdate=func.now())
+    rotated_on = Column(DateTime)
 
     cards = relationship('Card', backref='set')
+
 
 class Card(Base):
     __tablename__ = 'card'
 
     id = Column(Integer, primary_key=True)
     art_id = Column(Integer)
-    set_id = Column(String(3))
+    set_id = Column(String(3), ForeignKey('set.id'))
     title = Column(String(100), nullable=False, unique=True)
     rarity = Column(Text)
     power = Column(Text)
@@ -31,9 +34,10 @@ class Card(Base):
     created_on = Column(DateTime, server_default=func.now())
 
     types = relationship('CardType', backref='card')
-    subtypes = relationship('CardSubtype', backref='card')
-    costs = relationship('CardCost', backref='card')
-    abilities = relationship('CardAbility', backref='card')
+    # subtypes = relationship('CardSubtype', backref='card')
+    # costs = relationship('CardCost', backref='card')
+    # abilities = relationship('CardAbility', backref='card')
+
 
 class CardType(Base):
     __tablename__ = 'card_type'
@@ -43,6 +47,7 @@ class CardType(Base):
     type = Column(Text)
     created_on = Column(DateTime, server_default=func.now())
 
+
 class CardSubtype(Base):
     __tablename__ = 'card_subtype'
 
@@ -50,6 +55,7 @@ class CardSubtype(Base):
     card_id = Column(Integer, ForeignKey('card.id'))
     subtype = Column(Text)
     created_on = Column(DateTime, server_default=func.now())
+
 
 class CardCost(Base):
     __tablename__ = 'card_cost'
@@ -60,6 +66,7 @@ class CardCost(Base):
     cost = Column(Integer)
     created_on = Column(DateTime, server_default=func.now())
 
+
 class CardAbility(Base):
     __tablename__ = 'card_ability'
 
@@ -68,11 +75,14 @@ class CardAbility(Base):
     ability = Column(Text)
     created_on = Column(DateTime, server_default=func.now())
 
+
 def drop_all(engine):
     Base.metadata.drop_all(engine)
 
+
 def create_all(engine):
     Base.metadata.create_all(engine)
+
 
 if __name__ == '__main__':
     print(Card.__table__)
